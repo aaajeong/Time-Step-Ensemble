ce25f8829344fa1beed728481440e3307c9af0e4
aaajeong <qkrdkwjd59@naver.com>
2022-02-28 21:31:42 +0900

    Calculate Translation Quality Estimation for softvoting, survival, majoiry ensemble per 2epoch

--- Calculate_QE.py
+++ Calculate_QE.py
@@ -13,14 +13,14 @@ from tqe import TQE
 
 # In[ ]:
 
-
-wb = openpyxl.load_workbook('TQE(Epoch).xlsx')
-sheet = wb['model_5']
+# TQE 측정
+wb = openpyxl.load_workbook('TQE(5-Epoch).xlsx')
+sheet = wb['model_15']
 # source : 번역 원문
 # target : 기계 번역 문장
 
-for i in range(500, 1002):
-    target = []
+for i in range(500, 1002): 
+    target = [] 
     source = []
     candidate = sheet.cell(row = i, column = 19).value.capitalize()
     reference = sheet.cell(row = i, column = 3).value
@@ -34,8 +34,10 @@ for i in range(500, 1002):
     cos_sim_values = model.fit(source, target)
     sheet.cell(row = i, column = 20).value = cos_sim_values[0]
     
-wb.save('TQE(Epoch).xlsx')
+wb.save('TQE(5-Epoch).xlsx')
 
+
+#################################################################################
 
 # lang_1 = ["my son s name is tom ."]
 # lang_2 = ["El nombre de mi hijo es Tom."]
@@ -44,12 +46,13 @@ wb.save('TQE(Epoch).xlsx')
 # cos_sim_values = model.fit(lang_1, lang_2)
 # print(cos_sim_values)
 
+#################################################################################
 
 # In[ ]:
 
-
-# wb = openpyxl.load_workbook('TQE(Epoch).xlsx')
-# sheet = wb['model_1']
+# TQE 평균 값
+# wb = openpyxl.load_workbook('TQE(5-Epoch).xlsx')
+# sheet = wb['real']
 # qe = []
 
 
@@ -62,5 +65,19 @@ wb.save('TQE(Epoch).xlsx')
 # sheet.cell(row = 1002, column = 5).value = qe.mean()
 
 
-# wb.save('TQE(Epoch).xlsx')
+# wb.save('TQE(5-Epoch).xlsx')
 
+
+# no word 제거
+# wb = openpyxl.load_workbook('TQE(5-Epoch).xlsx')
+# sheet = wb['real']
+
+
+# for i in range(2, 1002):
+#     candidate = sheet.cell(row = i, column = 4).value
+#     if candidate == 'no word':
+#         sheet.cell(row = i, column = 5).value = 0
+
+
+
+# wb.save('TQE(5-Epoch).xlsx')
